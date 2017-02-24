3644491beccca56ff7bf50cd3cfdab22cb9a68f5
jared edwards <jredwards519@gmail.com>
2017-02-24 13:15:07 -0500

    all working except for create

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -8,18 +8,35 @@ class CharacterController < ApplicationController
     @character = Character.find(params[:id])
   end
 
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.create!(character_params)
+    redirect_to @character
+  end
+
   def edit
     @character = Character.find(params[:id])
   end
 
-  def new
-    @character = Character.create(params[:character])
+  def update
+    @character = Character.find(params[:id])
+    @character.update(character_params)
+
+    redirect_to @character
   end
 
   def destroy
     @character = Character.find(params[:id])
     @character.destroy
     redirect_to :root
+  end
+
+  private
+  def character_params
+    params.require(:character).permit(:name, :alive)
   end
 
 end
--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -8,22 +8,35 @@ class HouseController < ApplicationController
     @house = House.find(params[:id])
   end
 
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to @house
+  end
+
   def edit
     @house = House.find(params[:id])
   end
 
-  def new
-    @house = House.create(params[:house])
-  end
-
   def update
+    @house = House.find(params[:id])
+    @house.update(house_params)
 
+    redirect_to @house
   end
 
   def destroy
     @house = House.find(params[:id])
     @house.destroy
     redirect_to :root
+  end
+
+  private
+  def house_params
+    params.require(:house).permit(:name, :img_url)
   end
 
 end
