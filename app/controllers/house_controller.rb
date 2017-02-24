2a9379931b09b6ee0e56363b7dfde2c94bb73a7a
jared edwards <jredwards519@gmail.com>
2017-02-24 10:48:33 -0500

    basic views working with proper links

--- app/controllers/house_controller.rb
+++ app/controllers/house_controller.rb
@@ -1,10 +1,19 @@
 class HouseController < ApplicationController
+
   def index
+    @houses = House.all
   end
 
   def show
+    @house = House.find(params[:id])
   end
 
   def edit
+    @house = House.find(params[:id])
   end
+
+  def new
+    @house = House.create(params[:house])
+  end
+
 end
